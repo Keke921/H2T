f96309af41d1c8bada5c4c80e9ac25310a2ade51
李梦柯 （LI Mengke） <46624684+Keke921@users.noreply.github.com>
2024-10-14 14:56:35 +0800

    Update methods.py

--- methods.py
+++ methods.py
@@ -46,6 +46,27 @@ def H2T(x1, x2, rho = 0.3):
             x1[i][:,index,:,:] = x2[i][:,index,:,:]    
     return x1
 
+
+class H2Tv2(nn.Module):
+    def __init__(self, beta = 0.2, cls_num_list=None, use_cuda=True):
+        super().__init__()
+        self.beta = None
+        self.cls_num_list = cls_num_list
+        self.use_cuda = use_cuda
+        
+    def forward(self, x1, x2, dis, y1, y2): 
+
+        dis = 1/(1+dis) 
+        p = (dis-0.5)/0.5*0.4+0.6
+        
+        one_hot1 = F.one_hot(y1,p.size()[1]).float().to(y1.device)                 
+        p1 = (p*one_hot1).sum(dim=1)
+        p2 = 1-p1
+
+        x = torch.mul(x1, p1.unsqueeze(1)) + torch.mul(x2, p2.unsqueeze(1)) 
+        return x
+
+
 class LearnableWeightScaling(nn.Module):
     def __init__(self, num_classes):
         super(LearnableWeightScaling, self).__init__()
